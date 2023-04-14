e0eea155aff1cd072fa7d6ce57b198417d281306
Navdeep Khubber <navdeepkhubber16@gmail.com>
2023-04-14 12:37:39 +0530

    Counter App using React

---
+++ src/components/Counter.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const [userValue, setUserValue] = useState(1);
+  let colour = true;
+
+  const handleReset = () => {
+    setCount(0);
+  };
+  const handleIncrement = () => {
+    setCount(count + userValue);
+  };
+  const handleDecrement = () => {
+    setCount(count - userValue);
+  };
+  const handleUserInput = (e) => {
+    if (e.target.valueAsNumber) {
+      setUserValue(e.target.valueAsNumber);
+    }
+  };
+
+  if (count >= 0) {
+    colour = false;
+  } else {
+    colour = true;
+  }
+
+  return (
+    <div className="counter-container">
+      <h1>Counter App</h1>
+      <div className="counter">
+        <h1 className={colour ? "red" : "green"}>{count}</h1>
+        <div className="buttons">
+          <button onClick={handleIncrement}>Increment</button>
+          <button onClick={handleDecrement}>Decrement</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+        <div className="userInput-container">
+          <label className="userInput-label" htmlFor="userValue" />
+          Enter a value:
+          <input
+            type="number"
+            className="userInput"
+            name="userValue"
+            id="userValue"
+            onChange={handleUserInput}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
